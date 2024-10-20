11bfa0d0df9f0af4a285a1e35b945030b334f4fa
Christopher Freeley <freeley.c@husky.neu.edu>
2024-10-19 22:57:02 -0400

    full map

--- Asylum/balloon.gd
+++ Asylum/balloon.gd
@@ -139,7 +139,7 @@ func _on_balloon_gui_input(event: InputEvent) -> void:
   # See if we need to skip typing of the dialogue
   if dialogue_label.is_typing:
     var mouse_was_clicked: bool = event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()
-    var skip_button_was_pressed: bool = event.is_action_pressed(skip_action)
+    var skip_button_was_pressed: bool = event.is_action_pressed(skip_action) or event.is_action_pressed(next_action)
     if mouse_was_clicked or skip_button_was_pressed:
       get_viewport().set_input_as_handled()
       dialogue_label.skip_typing()
